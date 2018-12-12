837c0c2901673dba153810f74d2f6ca47cf379df
Marc Chakiachvili <mchakiachvili@ebi.ac.uk>
2018-12-12 16:58:45 +0000

    Updated scripts for manual launch

---
+++ scripts/load_phi_identifier.py
@@ -0,0 +1,79 @@
+#!/usr/bin/env python
+import argparse
+import logging
+import os
+import sys
+from os.path import expanduser
+
+from bio.ensembl.ontology.loader.db import dal
+from bio.ensembl.ontology.loader.models import Ontology, Term, Relation, RelationType, get_one_or_create
+
+# allow ols.py to be run from any path
+os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
+
+logging.basicConfig(level=logging.INFO,
+                    format='%(asctime)s - %(levelname)s - %(message)s',
+                    datefmt='%Y-%m-%d %H:%M:%S')
+
+logger = logging.getLogger(__name__)
+
+logging.getLogger('urllib3.connectionpool').setLevel(logging.WARNING)
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description='Produce a release calendar')
+    parser.add_argument('-v', '--verbose', help='Verbose output', action='store_true')
+    parser.add_argument('-e', '--release', type=int, required=True, help='Release number')
+    parser.add_argument('-u', '--host_url', type=str, required=False,
+                        help='Db Host Url format engine:///user:pass@host:port')
+
+    args = parser.parse_args(sys.argv[1:])
+    logger.setLevel(
+        logging.ERROR if args.verbose is None else logging.INFO if args.verbose is False else logging.DEBUG)
+    logging.getLogger('sqlalchemy.engine').setLevel(logging.WARNING)
+    logger.debug('Script arguments %s', args)
+    db_name = 'ensembl_ontology_{}'.format(args.release)
+    if args.host_url is None:
+        db_url = 'sqlite:///' + expanduser("~") + '/' + db_name + '.sqlite'
+    else:
+        db_url = '{}{}'.format(args.host_url, db_name)
+    logger.debug('Db Url set to %s', db_url)
+
+    response = input("Confirm to proceed (y/N)? ")
+
+    if response.upper() != 'Y':
+        logging.info('Process cancelled')
+        exit(0)
+    dal.db_init(db_url)
+    with dal.session_scope() as session:
+        ontologies = session.query(Ontology).filter_by(name='phi', namespace='phibase_identifier').all()
+        for ontology in ontologies:
+            logger.info('Deleting namespaced ontology %s - %s', ontology.name, ontology.namespace)
+            rel = session.query(Relation).filter_by(ontology=ontology).delete()
+            res = session.query(Term).filter_by(ontology=ontology).delete()
+            logger.info('Wiped %s Terms', res)
+            logger.debug('...Done')
+        m_ontology, created = get_one_or_create(Ontology, session,
+                                                name='phi',
+                                                namespace='phibase_identifier',
+                                                create_method_kwargs=dict(
+                                                    version='1.0',
+                                                    title='PHIBase identifier')
+                                                )
+        relation_type, created = get_one_or_create(RelationType,
+                                                   session,
+                                                   name='is_a')
+        for i in range(10000):
+            accession = 'PHI:{}'.format(i)
+            term = Term(accession=accession, name='PHI-Base identifier %s' % accession)
+            logger.debug('Adding Term %s', accession)
+            session.add(term)
+            m_ontology.terms.append(term)
+            if i != 0:
+                term.add_parent_relation(m_related, relation_type, session)
+            else:
+                m_related = term
+            if i % 100 == 0:
+                logger.info('Committing transaction')
+                session.commit()
+
+    logger.info('...Done')
--- scripts/loader.py
+++ scripts/loader.py
@@ -1,10 +1,14 @@
-#!/bin/bash
+#!/usr/bin/env python
 import argparse
 import logging
 import os
 import sys
+from os.path import expanduser
+
+
 from bio.ensembl.ontology.loader import OlsLoader
 from bio.ensembl.ontology.loader.db import dal
+
 # allow ols.py to be run from any path
 os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
@@ -19,26 +23,33 @@ logging.getLogger('urllib3.connectionpool').setLevel(logging.WARNING)
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Produce a release calendar')
     parser.add_argument('-v', '--verbose', help='Verbose output', action='store_true')
-    parser.add_argument('-o', '--ontology', help='Ontology short name', required=False, dest='ontology', default='all')
+    parser.add_argument('-o', '--ontology', help='Ontology short name', required=True, dest='ontology', default='all')
     parser.add_argument('-e', '--release', type=int, required=True, help='Release number')
     parser.add_argument('-k', '--keep', required=False, help='Keep database', action='store_true')
     parser.add_argument('-u', '--host_url', type=str, required=False,
                         help='Db Host Url format engine:///user:pass@host:port')
 
     arguments = parser.parse_args(sys.argv[1:])
-    logger.setLevel(logging.ERROR if arguments.verbose is False else logging.DEBUG)
+    logger.setLevel(
+        logging.ERROR if arguments.verbose is None else logging.INFO if arguments.verbose is False else logging.DEBUG)
     logging.getLogger('sqlalchemy.engine').setLevel(logging.WARNING)
     logger.debug('Script arguments %s', arguments)
     args = vars(parser.parse_args())
-    # db_name = 'ensembl_ontology_{}'.format(arguments.release)
-    db_name = 'ols_ontology'
+    db_name = 'ensembl_ontology_{}'.format(arguments.release)
+    options = {'drop': not arguments.keep, 'echo': arguments.verbose}
     if arguments.host_url is None:
-        db_url = 'sqlite:////' + os.path.dirname(os.path.realpath(__file__)) + '/' + db_name + '.sqlite'
+        db_url = 'sqlite:///' + expanduser("~") + '/' + db_name + '.sqlite'
+        options.update({'pool_size':None})
     else:
         db_url = '{}/{}'.format(arguments.host_url, db_name)
     logger.debug('Db Url set to %s', db_url)
-    options = {'drop': not arguments.keep, 'echo': arguments.verbose}
-    logger.debug('Loader arguments %s %s ', db_url, options)
+    logger.info('Loader arguments %s %s ', db_url, options)
+    response = input("Confirm to proceed (y/N)? ")
+
+    if response.upper() != 'Y':
+        logging.info('Process cancelled')
+        exit(0)
+
     loader = OlsLoader(db_url, **options)
 
     if not arguments.keep:
