dffa085b63eeb7dfbd4ccff62968607a96d1bd6c
Marc Chakiachvili <mchakiachvili@ebi.ac.uk>
2019-02-21 10:59:51 +0000

    Added schema patches (moved from ensembl repo) for schema version while creating database

--- sql/tables.sql
+++ sql/tables.sql
@@ -1,129 +1,136 @@
-
-CREATE TABLE meta (
-	meta_id INTEGER UNSIGNED NOT NULL AUTO_INCREMENT, 
-	meta_key VARCHAR(64) NOT NULL, 
-	meta_value VARCHAR(128), 
-	species_id INTEGER UNSIGNED DEFAULT NULL, 
-	PRIMARY KEY (meta_id)
-)ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE utf8_unicode_ci
-
-
-CREATE UNIQUE INDEX key_value_idx ON meta (meta_key, meta_value)
-
-CREATE TABLE ontology (
-	ontology_id INTEGER UNSIGNED NOT NULL AUTO_INCREMENT, 
-	name VARCHAR(64) NOT NULL, 
-	namespace VARCHAR(64) NOT NULL, 
-	data_version VARCHAR(64) DEFAULT NULL, 
-	title VARCHAR(255) DEFAULT NULL, 
-	PRIMARY KEY (ontology_id)
-)ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE utf8_unicode_ci
+-- Copyright [1999-2015] Wellcome Trust Sanger Institute and the EMBL-European Bioinformatics Institute
+-- Copyright [2016-2019] EMBL-European Bioinformatics Institute
+--
+-- Licensed under the Apache License, Version 2.0 (the "License");
+-- you may not use this file except in compliance with the License.
+-- You may obtain a copy of the License at
+--
+--      http://www.apache.org/licenses/LICENSE-2.0
+--
+-- Unless required by applicable law or agreed to in writing, software
+-- distributed under the License is distributed on an "AS IS" BASIS,
+-- WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+-- See the License for the specific language governing permissions and
+-- limitations under the License.
 
 
-CREATE UNIQUE INDEX ontology_name_namespace_idx ON ontology (name, namespace)
+-- ---------------------------------------------------------------------
+-- The schema for the ensembl_ontology_NN database.
+-- ---------------------------------------------------------------------
 
-CREATE TABLE subset (
-	subset_id INTEGER UNSIGNED NOT NULL AUTO_INCREMENT, 
-	name VARCHAR(64) NOT NULL, 
-	definition VARCHAR(511) NOT NULL DEFAULT '', 
-	PRIMARY KEY (subset_id), 
-	UNIQUE (name)
-)ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE utf8_unicode_ci
+CREATE TABLE `meta` (
+  `meta_id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `meta_key` varchar(64) NOT NULL,
+  `meta_value` varchar(128) DEFAULT NULL,
+  `species_id` int(10) unsigned DEFAULT NULL,
+  PRIMARY KEY (`meta_id`),
+  UNIQUE KEY `key_value_idx` (`meta_key`,`meta_value`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
+# Add schema type and schema version to the meta table
+INSERT INTO meta (meta_key, meta_value) VALUES
+  ('schema_type', 'ontology'),
+  ('schema_version', '97');
 
+# Patches included in this schema file
+INSERT INTO meta (meta_key, meta_value)
+  VALUES ('patch', 'patch_96_97_a.sql|schema_version');
 
-CREATE TABLE relation_type (
-	relation_type_id INTEGER UNSIGNED NOT NULL AUTO_INCREMENT, 
-	name VARCHAR(64) NOT NULL, 
-	PRIMARY KEY (relation_type_id), 
-	UNIQUE (name)
-)ENGINE=MyISAM
-
-
-
-CREATE TABLE term (
-	term_id INTEGER UNSIGNED NOT NULL AUTO_INCREMENT, 
-	ontology_id INTEGER UNSIGNED NOT NULL, 
-	subsets TEXT, 
-	accession VARCHAR(64) NOT NULL, 
-	name VARCHAR(255) COLLATE utf8_general_ci NOT NULL, 
-	definition TEXT COLLATE utf8_general_ci, 
-	is_root INTEGER NOT NULL DEFAULT 0, 
-	is_obsolete INTEGER NOT NULL DEFAULT 0, 
-	iri TEXT, 
-	PRIMARY KEY (term_id), 
-	FOREIGN KEY(ontology_id) REFERENCES ontology (ontology_id), 
-	UNIQUE (accession)
-)ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE utf8_unicode_ci
+CREATE TABLE `ontology` (
+  `ontology_id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `name` varchar(64) NOT NULL,
+  `namespace` varchar(64) NOT NULL,
+  `data_version` varchar(64) DEFAULT NULL,
+  `title` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`ontology_id`),
+  UNIQUE KEY `ontology_name_namespace_idx` (`name`,`namespace`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 
-CREATE UNIQUE INDEX term_ontology_acc_idx ON term (ontology_id, accession)
-CREATE INDEX term_name_idx ON term (name(100))
+CREATE TABLE `subset` (
+  `subset_id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `name` varchar(64) NOT NULL,
+  `definition` varchar(511) NOT NULL DEFAULT '',
+  PRIMARY KEY (`subset_id`),
+  UNIQUE KEY `name` (`name`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
-CREATE TABLE synonym (
-	synonym_id INTEGER UNSIGNED NOT NULL AUTO_INCREMENT, 
-	term_id INTEGER UNSIGNED NOT NULL, 
-	name TEXT COLLATE utf8_general_ci NOT NULL, 
-	type ENUM('EXACT','BROAD','NARROW','RELATED'), 
-	dbxref VARCHAR(500) DEFAULT NULL, 
-	PRIMARY KEY (synonym_id), 
-	FOREIGN KEY(term_id) REFERENCES term (term_id)
-)ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE utf8_unicode_ci
+CREATE TABLE `term` (
+  `term_id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `ontology_id` int(10) unsigned NOT NULL,
+  `subsets` text,
+  `accession` varchar(64) NOT NULL,
+  `name` varchar(255) COLLATE utf8_general_ci NOT NULL,
+  `definition` text COLLATE utf8_general_ci,
+  `is_root` int(11) NOT NULL DEFAULT 0,
+  `is_obsolete` int(11) NOT NULL DEFAULT 0,
+  `iri` text,
+  PRIMARY KEY (`term_id`),
+  UNIQUE KEY `accession` (`accession`),
+  UNIQUE KEY `term_ontology_acc_idx` (`ontology_id`,`accession`),
+  KEY `term_name_idx` (`name`(100))
+) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
+CREATE TABLE `synonym` (
+  `synonym_id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `term_id` int(10) unsigned NOT NULL,
+  `name` text COLLATE utf8_general_ci NOT NULL,
+  `type` enum('EXACT','BROAD','NARROW','RELATED') DEFAULT NULL,
+  `dbxref` varchar(500) DEFAULT NULL,
+  PRIMARY KEY (`synonym_id`),
+  UNIQUE KEY `synonym_term_idx` (`term_id`,`synonym_id`),
+  KEY `synonym_name_idx` (`name`(100))
+) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
-CREATE INDEX synonym_name_idx ON synonym (name(100))
-CREATE UNIQUE INDEX synonym_term_idx ON synonym (term_id, synonym_id)
+CREATE TABLE `alt_id` (
+  `alt_id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `term_id` int(10) unsigned NOT NULL,
+  `accession` varchar(64) NOT NULL,
+  PRIMARY KEY (`alt_id`),
+  UNIQUE KEY `term_alt_idx` (`term_id`,`alt_id`),
+  KEY `ix_alt_id_accession` (`accession`)
+) ENGINE=MyISAM;
 
-CREATE TABLE alt_id (
-	alt_id INTEGER UNSIGNED NOT NULL AUTO_INCREMENT, 
-	term_id INTEGER UNSIGNED NOT NULL, 
-	accession VARCHAR(64) NOT NULL, 
-	PRIMARY KEY (alt_id), 
-	FOREIGN KEY(term_id) REFERENCES term (term_id)
-)ENGINE=MyISAM
-
-
-CREATE UNIQUE INDEX term_alt_idx ON alt_id (term_id, alt_id)
-CREATE INDEX ix_alt_id_accession ON alt_id (accession)
-
-CREATE TABLE relation (
-	relation_id INTEGER UNSIGNED NOT NULL AUTO_INCREMENT, 
-	child_term_id INTEGER UNSIGNED NOT NULL, 
-	parent_term_id INTEGER UNSIGNED NOT NULL, 
-	relation_type_id INTEGER UNSIGNED NOT NULL, 
-	intersection_of TINYINT UNSIGNED NOT NULL DEFAULT 0, 
-	ontology_id INTEGER UNSIGNED NOT NULL, 
-	PRIMARY KEY (relation_id), 
-	FOREIGN KEY(child_term_id) REFERENCES term (term_id), 
-	FOREIGN KEY(parent_term_id) REFERENCES term (term_id), 
-	FOREIGN KEY(relation_type_id) REFERENCES relation_type (relation_type_id), 
-	FOREIGN KEY(ontology_id) REFERENCES ontology (ontology_id)
-)ENGINE=MyISAM
-
-
-CREATE INDEX ix_relation_ontology_id ON relation (ontology_id)
-CREATE INDEX ix_relation_parent_term_id ON relation (parent_term_id)
-CREATE UNIQUE INDEX child_parent_idx ON relation (child_term_id, parent_term_id, relation_type_id, intersection_of, ontology_id)
-CREATE INDEX ix_relation_relation_type_id ON relation (relation_type_id)
-
-CREATE TABLE closure (
-	closure_id INTEGER UNSIGNED NOT NULL AUTO_INCREMENT, 
-	child_term_id INTEGER UNSIGNED NOT NULL, 
-	parent_term_id INTEGER UNSIGNED NOT NULL, 
-	subparent_term_id INTEGER UNSIGNED, 
-	distance TINYINT UNSIGNED NOT NULL, 
-	ontology_id INTEGER UNSIGNED NOT NULL, 
-	confident_relationship BOOL NOT NULL DEFAULT 0, 
-	PRIMARY KEY (closure_id), 
-	FOREIGN KEY(child_term_id) REFERENCES term (term_id), 
-	FOREIGN KEY(parent_term_id) REFERENCES term (term_id), 
-	FOREIGN KEY(subparent_term_id) REFERENCES term (term_id), 
-	FOREIGN KEY(ontology_id) REFERENCES ontology (ontology_id), 
-	CHECK (confident_relationship IN (0, 1))
-)ENGINE=MyISAM
+CREATE TABLE `relation_type` (
+  `relation_type_id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `name` varchar(64) NOT NULL,
+  PRIMARY KEY (`relation_type_id`),
+  UNIQUE KEY `name` (`name`)
+) ENGINE=MyISAM;
 
 
-CREATE INDEX parent_subparent_idx ON closure (parent_term_id, subparent_term_id)
-CREATE INDEX ix_closure_subparent_term_id ON closure (subparent_term_id)
-CREATE INDEX ix_closure_ontology_id ON closure (ontology_id)
-CREATE UNIQUE INDEX closure_child_parent_idx ON closure (child_term_id, parent_term_id, subparent_term_id, ontology_id)
+CREATE TABLE `relation` (
+  `relation_id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `child_term_id` int(10) unsigned NOT NULL,
+  `parent_term_id` int(10) unsigned NOT NULL,
+  `relation_type_id` int(10) unsigned NOT NULL,
+  `intersection_of` tinyint(1) NOT NULL,
+  `ontology_id` int(10) unsigned NOT NULL,
+  PRIMARY KEY (`relation_id`),
+  UNIQUE KEY `child_parent_idx` (`child_term_id`,`parent_term_id`,`relation_type_id`,`intersection_of`,`ontology_id`),
+  KEY `ix_relation_parent_term_id` (`parent_term_id`),
+  KEY `ix_relation_relation_type_id` (`relation_type_id`),
+  KEY `ix_relation_ontology_id` (`ontology_id`)
+) ENGINE=MyISAM;
+
+
+CREATE TABLE `closure` (
+  `closure_id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `child_term_id` int(10) unsigned NOT NULL,
+  `parent_term_id` int(10) unsigned NOT NULL,
+  `subparent_term_id` int(10) unsigned DEFAULT NULL,
+  `distance` tinyint(3) unsigned NOT NULL,
+  `ontology_id` int(10) unsigned NOT NULL,
+  `confident_relationship` tinyint(1) NOT NULL,
+  PRIMARY KEY (`closure_id`),
+  UNIQUE KEY `closure_child_parent_idx` (`child_term_id`,`parent_term_id`,`subparent_term_id`,`ontology_id`),
+  KEY `ix_closure_subparent_term_id` (`subparent_term_id`),
+  KEY `ix_closure_ontology_id` (`ontology_id`),
+  KEY `parent_subparent_idx` (`parent_term_id`,`subparent_term_id`)
+) ENGINE=MyISAM;
+
+-- There are additional tables in the released databases called
+-- "aux_XX_YY_map".  These are created by the "add_subset_maps.pl"
+-- scripts.  Please see the README document for further information.
+
+-- $Id$
