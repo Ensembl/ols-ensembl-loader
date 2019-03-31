d41c077b2cee0a8a13a7a5557f66402b417bba13
Marc Chakiachvili <mchakiachvili@ebi.ac.uk>
2019-03-31 18:06:16 +0100

    Subset table column 'definition' still not long enough

--- bio/ensembl/ontology/loader/models.py
+++ bio/ensembl/ontology/loader/models.py
@@ -205,7 +205,7 @@ class Subset(LoadAble, Base):
 
     subset_id = Column(UnsignedInt, primary_key=True)
     name = Column(String(64, convert_unicode=True), nullable=False, unique=True)
-    definition = Column(Unicode(511), nullable=False, server_default=text("''"))
+    definition = Column(BigStringUtf8, nullable=False, server_default=text("''"))
 
 
 class Term(LoadAble, Base):
